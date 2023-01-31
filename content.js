9893d0bf2f9a0a3f89a4b84fc3820cd45e3765b0
ntntr <121062441+ntntr@users.noreply.github.com>
2023-01-31 15:47:56 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,22 @@
+// content.js
+
+document.body.addEventListener("DOMSubtreeModified", function() {
+  let elements = document.getElementsByTagName("*");
+  
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+
+    for (let j = 0; j < element.childNodes.length; j++) {
+      let node = element.childNodes[j];
+
+      if (node.nodeType === 3) {
+        let text = node.nodeValue;
+        let replacedText = text.replace(/Account suspended/gi, "Account floyded");
+
+        if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+        }
+      }
+    }
+  }
+});
